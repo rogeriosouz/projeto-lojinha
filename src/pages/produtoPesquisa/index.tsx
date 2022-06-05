2c00e6384d66a1b5270d5ea6d9fdede5e5fa14d4
rogerio pereria <rogeriopereriradesouzagomes@gmail.com>
2022-06-05 17:09:40 -0300

    adicionando pag de pesquisa de categorias e pesquisa

--- src/pages/produtoPesquisa/index.tsx
+++ src/pages/produtoPesquisa/index.tsx
@@ -1,14 +1,64 @@
-import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { Link, useParams } from 'react-router-dom';
+import { Header } from '../../components/header';
+import { ProdutosPage } from '../../components/produtosPage';
+import axios from '../../services/axios';
+
+import {
+  PesquisaProdutoSection,
+  Conteudo
+} from './style';
+
+type Produtos = {
+  name: string,
+  prace: number,
+  descricao: string,
+  categoria: string
+}
 
 export function ProdutosPesquisa() {
-  const { categoria } = useParams();
+  const { categoria }  = useParams<string>();
   const cate = categoria?.replace(':', '');
 
+  const { data, isFetching } = useQuery(['pesquisaProdutos', categoria], async () => {
+    const response = await axios.get('/produto');
+    const produtos: Produtos[] = [];
+    response.data.map((item: Produtos) => {
+      if(item.categoria === cate) {
+        produtos.push(item);
+      }
+    })
+    return produtos  
+  }, {
+    staleTime: 1000 * 60 // 1 minuto
+  })
+
+
   return (
-    <section style={{ marginTop: '60px' }}>
-      <div>
-        {cate}
-      </div>
-    </section>
+    <> 
+      <Header />
+      <PesquisaProdutoSection>
+        <Conteudo>
+          {isFetching ? (
+            <p><strong>Caregando...</strong></p>
+          ): (
+            <div>
+              {data?.map((item: Produtos) => (
+                <Link 
+                  to={`/produto/:${item.name}`} 
+                  key={item.name}
+                >
+                  <ProdutosPage 
+                    name={item.name}
+                    price={item.prace}
+                    img='none'
+                  />
+                </Link>
+              ))}
+            </div>
+          )}
+        </Conteudo>
+      </PesquisaProdutoSection>
+    </>
   );
 }
--- src/pages/produtoPesquisa/style.ts
+++ src/pages/produtoPesquisa/style.ts
@@ -1,1 +1,29 @@
 import styled from 'styled-components';
+
+export const PesquisaProdutoSection = styled.section`
+  width: 100%;
+  min-height: 100vh;
+  margin-top: 50px; 
+  background-color: #dfdfdf;
+  padding-bottom: 40px;
+`;
+
+
+export const Conteudo = styled.div`
+  max-width: 600px;
+  display: grid;
+  padding-top: 20px;
+  grid-template-columns: 1fr 1fr 1fr;
+  margin: auto;
+  gap: 10px;
+
+  padding: 10px;
+
+  @media (max-width: 648px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (max-width: 437px) {
+    grid-template-columns: 1fr;
+  }
+`;
